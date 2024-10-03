6ea1f0aec0332025e3156d2357357ecdfd93423b
denghe <denghe@hotmail.com>
2024-10-03 15:06:14 +0800

    add some net code

---
+++ xx_asio_ioc.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include <xx_data_shared.h>
+#include <xx_ptr.h>
+
+#include <boost/asio.hpp>
+#include <boost/asio/as_tuple.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+namespace asio = boost::asio;
+using namespace asio::experimental::awaitable_operators;
+constexpr auto use_nothrow_awaitable = asio::as_tuple(asio::use_awaitable);
+using asio::co_spawn;
+using asio::awaitable;
+using asio::detached;
+
+namespace xx {
+	// sleep for coroutine
+	inline awaitable<void> Timeout(std::chrono::steady_clock::duration d) {
+		asio::steady_timer t(co_await asio::this_coro::executor);
+		t.expires_after(d);
+		co_await t.async_wait(use_nothrow_awaitable);
+	}
+
+
+	// server or worker's base class. need to ensure the longest life cycle
+	struct IOCBase : asio::io_context {
+		using asio::io_context::io_context;
+
+		// start listen an tcp port
+		template<typename SocketHandler>
+		void Listen(uint16_t const& port, SocketHandler&& sh) {
+			co_spawn(*this, [this, port, sh = std::move(sh)]()->awaitable<void> {
+				asio::ip::tcp::acceptor acceptor(*this, { asio::ip::tcp::v6(), port });
+				acceptor.set_option(asio::ip::tcp::acceptor::reuse_address(true));
+				while (!stopped()) {
+					asio::ip::tcp::socket socket(*this);
+					if (auto [ec] = co_await acceptor.async_accept(socket, use_nothrow_awaitable); !ec) {
+						sh(std::move(socket));
+					}
+					else break;
+				}
+			}, detached);
+		}
+	};
+
+	template<typename EndPoint = asio::ip::tcp::endpoint, typename S>
+	EndPoint AddressToEndpoint(S&& ipstr, asio::ip::port_type port) {
+		return EndPoint(asio::ip::address::from_string(ipstr), port);
+	}
+}
+
+// used to force convert "this" to derived type
+#define PEERTHIS ((PeerDeriveType*)(this))
+
+// member func exists checkers
+template<typename T> concept Has_Start_ = requires(T t) { t.Start_(); };
+template<typename T> concept Has_Stop_ = requires(T t) { t.Stop_(); };
---
+++ xx_asio_tcp_base.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+#include <xx_asio_ioc.h>
+
+namespace xx {
+
+	// tips: need wrap by xx::Shared, co_spawn hold for ensure life cycle
+	template<typename PeerDeriveType, typename IOCType, size_t writeQueueSizeLimit = 50000>
+	struct PeerTcpBaseCode : asio::noncopyable {
+		IOCType& ioc;
+		asio::ip::tcp::socket socket;
+		asio::steady_timer writeBlocker;
+		std::deque<xx::DataShared> writeQueue;
+		bool stoped = true;
+		std::string lastTcpBaseErr;
+
+		explicit PeerTcpBaseCode(IOCType& ioc_) : ioc(ioc_), socket(ioc_), writeBlocker(ioc_) {}
+		PeerTcpBaseCode(IOCType& ioc_, asio::ip::tcp::socket&& socket_) : ioc(ioc_), socket(std::move(socket_)), writeBlocker(ioc_) {}
+
+		// will be call after peer created
+		void Start() {
+			if (!stoped) return;
+			stoped = false;
+			writeBlocker.expires_at(std::chrono::steady_clock::time_point::max());
+			writeQueue.clear();
+			co_spawn(ioc, PEERTHIS->Read(xx::SharedFromThis(PEERTHIS)), detached);	// call PeerDeriveType's member func: awaitable<void> Read()
+			co_spawn(ioc, PEERTHIS->Write(xx::SharedFromThis(PEERTHIS)) , detached);
+			if constexpr (Has_Start_<PeerDeriveType>) {
+				PEERTHIS->Start_();
+			}
+		}
+
+		bool IsStoped() const {
+			return stoped;
+		}
+
+		bool Stop() {
+			if (stoped) return false;
+			stoped = true;
+			socket.close();
+			writeBlocker.cancel();
+			if constexpr (Has_Stop_<PeerDeriveType>) {
+				PEERTHIS->Stop_();
+			}
+			return true;
+		}
+
+		// tips: broadcast, DataShared is recommend
+		template<typename D>
+		void Send(D&& d) {
+			static_assert(std::is_base_of_v<xx::Data_r, std::decay_t<D>> || std::is_same_v<std::decay_t<D>, xx::DataShared>);
+			if (stoped) return;
+			if (!d) return;
+			writeQueue.emplace_back(std::forward<D>(d));
+			writeBlocker.cancel_one();
+		}
+
+		// for client dial connect to server only
+		awaitable<int> Connect(asio::ip::tcp::endpoint ep, std::chrono::steady_clock::duration d = 5s) {
+			if (!stoped) co_return 1;
+			socket = asio::ip::tcp::socket(ioc);    // for macos
+			auto r = co_await(socket.async_connect(ep, use_nothrow_awaitable) || Timeout(d));
+			if (r.index()) co_return 2;
+			if (auto& [e] = std::get<0>(r); e) co_return 3;
+			Start();
+			co_return 0;
+		}
+		awaitable<int> Connect(asio::ip::address ip, uint16_t port, std::chrono::steady_clock::duration d = 5s) {
+			co_return co_await Connect({ ip, port }, d);
+		}
+
+	protected:
+
+		// continues send data in writeQueue
+		awaitable<void> Write(auto memHolder) {
+			while (socket.is_open()) {
+				if (writeQueue.empty()) {
+					co_await writeBlocker.async_wait(use_nothrow_awaitable);
+					if (stoped) co_return;
+				}
+				if constexpr (writeQueueSizeLimit) {
+					if (writeQueue.size() > writeQueueSizeLimit) {
+						lastTcpBaseErr = "writeQueue.size() > " + std::to_string(writeQueueSizeLimit);
+						break;
+					}
+				}
+				auto& msg = writeQueue.front();
+				auto buf = msg.GetBuf();
+				auto len = msg.GetLen();
+			LabBegin:
+				auto [ec, n] = co_await asio::async_write(socket, asio::buffer(buf, len), use_nothrow_awaitable);
+				if (ec) {
+					lastTcpBaseErr = "asio::async_write error: " + std::string(ec.message());
+					break;
+				}
+				if (stoped) co_return;
+				if (n < len) {
+					len -= n;
+					buf += n;
+					goto LabBegin;
+				}
+				writeQueue.pop_front();
+			}
+			Stop();
+		}
+	};
+}
