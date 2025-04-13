cb182234d0c64bd6a4b856f0374a9b18b12b73e6
denghe <denghe@hotmail.com>
2025-04-14 04:30:34 +0800

    enhance

--- xx2d_scale9.h
+++ xx2d_scale9.h
@@ -6,12 +6,19 @@ namespace xx {
 	struct Scale9 {
 		Ref<Frame> frame;
 		UVRect center{};
+		XY centerAnchor = { 0, 1 };
+		XY centerScale{ 1, 1 };
+		XY centerOffset{};
 		XY texScale{ 1, 1 };
 		XY size{};
 		XY pos{};
+		XY anchor{};
+		float radians{};		// clockwise
 		float colorplus{ 1 };
 		RGBA8 color{ 255, 255, 255, 255 };
 
+		// return center pos
+		template<bool drawCenter = true, bool enableRotation = true>
 		void Draw() {
 			auto qs = EngineBase1::Instance().ShaderBegin(EngineBase1::Instance().shaderQuadInstance).Draw(frame->tex->GetValue(), 9);
 
@@ -48,92 +55,154 @@ namespace xx {
 			float py1 = size.y;
 			float py2 = size.y - (th1 * texScale.y);
 			float py3 = size.y - (size.y - th3 * texScale.y);
+
+			auto offset = pos;
+
+			AffineTransform at;
+			if constexpr (enableRotation) {
+				at = AffineTransform::MakePosScaleRadiansAnchorSize({}, 1, radians, anchor * size);
+			}
+			else {
+				radians = 0;
+				offset -= anchor * size;
+			}
+
 			{
 				auto& q = qs[0];
-				q.pos = pos + XY{ px1, py1 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px1, py1 });
+				}
+				else {
+					q.pos = offset + XY{ px1, py1 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = texScale;
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx1, ty1, tw1, th1 };
 			}
 			{
 				auto& q = qs[1];
-				q.pos = pos + XY{ px2, py1 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px2, py1 });
+				}
+				else {
+					q.pos = offset + XY{ px2, py1 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = { sx, texScale.y };
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx2, ty1, tw2, th1 };
 			}
 			{
 				auto& q = qs[2];
-				q.pos = pos + XY{ px3, py1 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px3, py1 });
+				}
+				else {
+					q.pos = offset + XY{ px3, py1 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = texScale;
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx3, ty1, tw3, th1 };
 			}
 			{
 				auto& q = qs[3];
-				q.pos = pos + XY{ px1, py2 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px1, py2 });
+				}
+				else {
+					q.pos = offset + XY{ px1, py2 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = { texScale.x, sy };
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx1, ty2, tw1, th2 };
 			}
 			{
 				auto& q = qs[4];
-				q.pos = pos + XY{ px2, py2 };
-				q.anchor = { 0, 1 };
-				q.scale = { sx, sy };
-				q.radians = {};
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px2 + centerOffset.x + tw2 * centerScale.x * centerAnchor.x, py2 + centerOffset.y + th2 * centerScale.y * (1 - centerAnchor.y) });
+				}
+				else {
+					q.pos = offset + XY{ px2 + centerOffset.x + tw2 * centerScale.x * centerAnchor.x, py2 + centerOffset.y + th2 * centerScale.y * (1 - centerAnchor.y) };
+				}
+				q.anchor = centerAnchor;
+				if (drawCenter) {
+					q.scale = { sx * centerScale.x, sy * centerScale.y };
+				}
+				else {
+					q.scale = {};
+				}
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx2, ty2, tw2, th2 };
 			}
 			{
 				auto& q = qs[5];
-				q.pos = pos + XY{ px3, py2 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px3, py2 });
+				}
+				else {
+					q.pos = offset + XY{ px3, py2 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = { texScale.x, sy };
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx3, ty2, tw3, th2 };
 			}
 			{
 				auto& q = qs[6];
-				q.pos = pos + XY{ px1, py3 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px1, py3 });
+				}
+				else {
+					q.pos = offset + XY{ px1, py3 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = texScale;
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx1, ty3, tw1, th3 };
 			}
 			{
 				auto& q = qs[7];
-				q.pos = pos + XY{ px2, py3 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px2, py3 });
+				}
+				else {
+					q.pos = offset + XY{ px2, py3 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = { sx, texScale.y };
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx2, ty3, tw2, th3 };
 			}
 			{
 				auto& q = qs[8];
-				q.pos = pos + XY{ px3, py3 };
+				if constexpr (enableRotation) {
+					q.pos = offset + at({ px3, py3 });
+				}
+				else {
+					q.pos = offset + XY{ px3, py3 };
+				}
 				q.anchor = { 0, 1 };
 				q.scale = texScale;
-				q.radians = {};
+				q.radians = radians;
 				q.colorplus = colorplus;
 				q.color = color;
 				q.texRect = { tx3, ty3, tw3, th3 };
